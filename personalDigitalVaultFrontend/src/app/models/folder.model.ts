a8c38aa41a2fd72af8f0e7da734b02fbd52aa181
thushaonline <rajthusha99@gmail.com>
2026-09-12 14:05:53 +0530

    Remove duplicate folder files

--- personalDigitalVaultFrontend/src/app/models/folder.model.ts
+++
@@ -1,9 +0,0 @@
-export interface FolderItem {
-  id: number;
-  name: string;
-  parentFolderId?: number | null;
-  createdAt: string;
-  documentCount: number;
-  credentialCount: number;
-  subFolderCount: number;
-}
